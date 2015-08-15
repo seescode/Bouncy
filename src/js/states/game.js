6cf208bb6e7e07b966bb791e086f260d82a86d36
seescode <seesyong@gmail.com>
2015-08-14 22:09:47 -0400

    Added physics group

--- src/js/states/game.js
+++ src/js/states/game.js
@@ -22,10 +22,36 @@ Game.prototype = {
     this.layer.resizeWorld();
     //this.layer.wrap = true;
     
+    this.map.createFromTiles(18, null, 'coins', 'Tile Layer 1');
+    
 
     this.text = this.game.add.text(10, 10, '0 POINTS', { font: "12px Arial", fill: "#ff0044" });
 
-    this.box = this.game.add.sprite(750, 150, 'box');
+
+    this.boxes = this.game.add.group();
+    this.boxes.enableBody = true;
+    this.physicsBodyType = Phaser.Physics.ARCADE;
+    
+
+    //this.box = this.game.add.sprite(750, 150, 'box');
+    var box = this.boxes.create(750, 150, 'box');
+    box.name = 'a';
+    box.body.collideWorldBounds = true;
+    box.body.bounce.set(1);
+    box.body.velocity.x = -300;
+    
+    box = this.boxes.create(500, 150, 'box');
+    box.name = 'b';
+    box.body.collideWorldBounds = true;
+    box.body.bounce.set(1);
+    box.body.velocity.x = -150;
+
+    box = this.boxes.create(300, 150, 'box');
+    box.name = 'c';
+    box.body.collideWorldBounds = true;
+    box.body.bounce.set(1);
+    box.body.velocity.x = -150;
+    
     
     this.qumark = this.game.add.sprite(100, 100, 'spriteSheet');
     
@@ -34,34 +60,34 @@ Game.prototype = {
     
     this.qumark.animations.play('left');
 
-    this.game.physics.arcade.enable(this.box);
+    //this.game.physics.arcade.enable(this.box);
     this.game.physics.arcade.enable(this.qumark);
     
     this.game.physics.arcade.gravity.set(0, 500);
     this.game.camera.follow(this.qumark);
 
 
-    this.box.body.collideWorldBounds = true;
+    //this.box.body.collideWorldBounds = true;
     this.qumark.body.collideWorldBounds = true;
 
-    this.box.body.bounce.set(1);
+    //this.box.body.bounce.set(1);
     this.qumark.body.bounce.set(1);
 
-    this.box.body.velocity.x = -150;
+    //this.box.body.velocity.x = -150;
     
     //this says make tile 4,5,14,15 be collidable
     //Note that the texture map starts from index 1.
     this.map.setCollisionBetween(4, 5);
     this.map.setCollisionBetween(14, 15);
     
-    var killCoin = function(player, coin) {
-      //this.map.replace(1, 18, 1, 1);
-      //coin.index = 1;
-      coin.copy(this.map.getTile(1, 1));
-    };
-
-    this.map.setTileIndexCallback(18, killCoin, this); 
-
+//     var killCoin = function(player, coin) {
+//       //this.map.replace(1, 18, 1, 1);
+//       //coin.index = 1;
+//       coin.copy(this.map.getTile(1, 1));
+//     };
+// 
+//     this.map.setTileIndexCallback(18, killCoin, this); 
+// 
     this.cursors = this.game.input.keyboard.createCursorKeys();
   },
 
@@ -70,7 +96,9 @@ Game.prototype = {
     
     this.game.physics.arcade.collide(this.qumark, this.layer);
     
-    this.game.physics.arcade.collide(this.qumark, this.box, function () {
+    this.game.physics.arcade.collide(this.boxes, this.layer);
+    
+    this.game.physics.arcade.collide(this.qumark, this.boxes, function () {
       me.game.state.start("GameOver");
     });
 
